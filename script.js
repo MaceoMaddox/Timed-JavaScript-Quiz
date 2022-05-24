f51975a6da3e11e5e7650657285d1f0a988b26b0
SonSonder <maceomaddox@gmail.com>
2022-05-23 20:13:40 -0400

    retrieved the DOM elements I needed

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //retrieve DOM elements
 const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question")
+var startBtn = document.getElementById("start-btn")
+const nextBtn = document.getElementById("next-btn")
+var shuffleArray = "";
+var currentString = "";
+const answerButtonsEl = document.getElementById("answer-buttons");
