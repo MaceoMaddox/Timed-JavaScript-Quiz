843d75339ea4e86b720bf7cc0d263d2031675727
SonSonder <maceomaddox@gmail.com>
2022-05-23 21:12:59 -0400

    I think I'm doing all of this back wards. But. making sure the quiz advances when a question is answered

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 //retrieve DOM elements
+var startBtn = document.getElementById("start-btn")
+var currentQuestion = "";
 const questionContainer = document.getElementById("question-container");
 const questionElement = document.getElementById("question")
-var startBtn = document.getElementById("start-btn")
 const nextBtn = document.getElementById("next-btn")
-var shuffleArray = "";
-var currentString = "";
+
 const answerButtonsEl = document.getElementById("answer-buttons");
+
+//create the buttons for the quiz
+// document.getElementById("startBtn").addEventListener("click", startQuiz);
+// nextBtn.addEventListener("click", () => {
+//     currentString++;
+//     nextString();
+// })
+
+//lay the groundwork for what occurs at the beginning of quiz
+function startQuiz() {
+    console.log("Begin Quiz");
+}
+
+//advance to next question
+function nextQuestion() {
+    showQuestion (currentQuestion)
+}
+
+//reflect whether or not an answer is correct when moving forward
+function showQuestion(currentQuestion) {
+    resetState();
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", chooseAnswer);
+        answerButtonsEl.appendChild("button")
+    })
+}
+
+//create responsiveness for the answering of the question
+function chooseAnswer(i) {
+    const selectedButton = i.target;
+    const correct = selectedButton.dataset.correct;
+}
